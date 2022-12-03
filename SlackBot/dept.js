3d7fa2d22b584cafd42b51d4e26b4e77601c8fa1
chaanheeLEE <chanhee1519@gmail.com>
2022-12-03 17:26:46 +0900

    Feat : answer similar dept --feature3

--- SlackBot/dept.js
+++ SlackBot/dept.js
@@ -1,14 +1,7 @@
-const fs = require('fs');
+const stringSimilarity = require('string-similarity');
+const readFile = require('./readFile');
 
-let dataBuffer;
-try {
-  dataBuffer = fs.readFileSync(`${__dirname}/office.json`).toString('utf-8');
-} catch (err) {
-  console.error(err);
-}
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-const arr = data.deptOffice;
+const { deptOffice } = readFile('office.json');
 
 const findDeptOffice = function (rtm, text, channel) {
   console.log('학과 사무실을 찾습니다.');
@@ -17,23 +10,36 @@ const findDeptOffice = function (rtm, text, channel) {
   try {
     let res;
 
-    for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i].dept === text) {
-        res = `${arr[i].buildingName}`;
-        if (arr[i].buildingNumber !== '0') {
-          res += ` No. ${arr[i].buildingNumber}`;
+    for (let i = 0; i < deptOffice.length; i += 1) {
+      if (deptOffice[i].dept.toLowerCase().replace(/ /g, '') === text.toLowerCase().replace(/ /g, '')) {
+        res = `${deptOffice[i].buildingName}`;
+        if (deptOffice[i].buildingNumber !== '0') {
+          res += ` No. ${deptOffice[i].buildingNumber}`;
         }
-        res += `, room: ${arr[i].roomNumber}`;
+        res += `, room: ${deptOffice[i].roomNumber}`;
         break;
       }
     }
-
     if (res === undefined) {
-      console.log(`${text}는 없는 학과입니다.`);
+      let answer;
+      let max = 0;
+      for (let i = 0; i < deptOffice.length; i += 1) {
+        const point = stringSimilarity.compareTwoStrings(text, deptOffice[i].dept);
+        if (point > max) {
+          max = point;
+          answer = deptOffice[i].dept;
+          res = `${deptOffice[i].buildingName}`;
+          if (deptOffice[i].buildingNumber !== '0') {
+            res += ` No. ${deptOffice[i].buildingNumber}`;
+          }
+          res += `, room: ${deptOffice[i].roomNumber}`;
+        }
+      }
+      rtm.sendMessage(`${answer}을 말씀하시는 건가요?\n${res} 입니다.`, channel);
+      console.log(`${text}와 유사한 답 출력`);
       return false;
     }
     rtm.sendMessage(`The office of ${text} is located in: ${res}`, channel);
-
     return Promise.resolve(`success: ${res}`);
   } catch (error) {
     console.log('error!', error.data);
