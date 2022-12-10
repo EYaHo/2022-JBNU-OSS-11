b60c241af3ba8c15d5d3b826eb0fb058a525d674
chaanheeLEE <chanhee1519@gmail.com>
2022-12-10 17:51:26 +0900

    Fix : Fix timeout --feature3

--- SlackBot/menu.spec.js
+++ SlackBot/menu.spec.js
@@ -29,9 +29,10 @@ selector.forEach((value) => {
   describe('메뉴 테스트를 시작합니다.', async () => {
     let res;
     before(async () => { res = await webScraping(url, value); });
-    it('메뉴 selector 테스트', (done) => {
+    it('메뉴 selector 테스트', function (done) {
+      this.timeout(10000);
       assert.equal(res, 0);
       done();
-    }).timeout(20000);
+    });
   });
 });
